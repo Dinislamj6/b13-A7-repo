ad17be2b6e7cf395cb64fe5255420c22d3ba188d
dinislamj6 <dinislamj6@gmail.com>
2026-04-15 22:41:25 +0600

    added filter timeline

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import { TimelineContext } from "../context/context";
 import Image from "next/image";
 import call from "../../assets/call.png"
@@ -9,34 +9,52 @@ import massage from "../../assets/text.png"
 
 const TimelinePage = () => {
     const { timeline, setTimeline } = useContext(TimelineContext);
+    const [filterData,setFilterData] = useState(timeline)
+    console.log(timeline);
+    const filterDate = (item) => {
+         const data = timeline.filter(el => el.type === item)
+         setFilterData(data)
+    }
     return (
         <div className="max-w-5xl mx-auto mt-10">
             <div>
                 <h2 className="font-bold text-2xl">Timeline </h2>
 
-                  {/* card part */}
+                {/* card part */}
+                <select onChange={(e) => filterDate(e.target.value)} defaultValue="Filter timeline" className="select select-ghost border shadow-3xl border-gray-300 mt-2">
+                    <option disabled={true}>Filter timeline</option>
+                    <option value={"call"}>call</option>
+                    <option value={"video"}>video</option>
+                    <option value={"text"}>text</option>
+                </select>
 
-                  {
-                    timeline.map(friend => <div key={friend.id}>
-                        <div className="card w-full bg-base-100 card-xs shadow-sm mt-4">
-                         <Image src={friend.type === "call"? call: friend.type === "video"? video: massage } alt="friend images"></Image>
-                          <h2>{friend.name}</h2>   
-                          <h2>{friend.date.toLocaleDateString()}</h2>   
-                          <h2>{friend.type}</h2>   
+                {
+                    filterData.map(friend => <div key={friend.id}>
+                        <div className="card w-full bg-base-100 card-xs shadow-sm mt-4 p-2">
+                            <div className="flex gap-3">
+                                <div>
+                                    <Image src={friend.type === "call" ? call : friend.type === "video" ? video : massage} alt="friend images"></Image>
+                                </div>
+                                <div>
+                                    <h2>{friend.name} with {friend.type}</h2>
+                                    <h2>{friend.date.toLocaleDateString()}</h2>
+                                </div>
+                            </div>
+
                         </div>
-                </div>)
-                  }
-
-              
-               
+                    </div>)
+                }
 
 
 
 
-                
+
+
+
+
             </div>
         </div>
     );
-  };
+};
 
 export default TimelinePage;
